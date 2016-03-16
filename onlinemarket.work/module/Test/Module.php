3a83b44a027fe68251bf3654d41dc3f089d90bf7
zend <zend@zf2f.zf2f.local>
2016-03-15 19:08:02 -0700

    2016-03-16 DB: added mod 2 lab files + source code

---
+++ onlinemarket.work/module/Test/Module.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Test;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        //$eventManager->trigger('whatever', $this, ['abc' => 'ABC']);
+    }
+
+}
